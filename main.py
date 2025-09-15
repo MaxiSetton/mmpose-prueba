2521a7c27a5988f33f5c8cec0d246e796d6cfc4a
Federico Diaz Nemeth <fdiaznemeth@gmail.com>
2025-09-15 20:33:39 -0300

    feat(api): update keypoints shape to support 12 frames and print output
    Signed-off-by: Federico Diaz Nemeth <fdiaznemeth@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+# in main.py
+
 from model import Predictor
-
-
-#Conseguir los keypoints a partir de un video con shape (frames, 133,3)
-
-#De ejemplo
 import numpy as np
-keypoints= np.zeros(shape=(10,133,3))
-checkpoint_path='best.pth'
-device='cpu'
-cfg_path='phoenix-2014t.yaml'
-predictor= Predictor(cfg_path,checkpoint_path,device)
-glosas=predictor(keypoints)
+
+# Change the number of frames from 10 to 12 (or any multiple of 4)
+# Also, keep the dtype from our previous fix!
+keypoints = np.zeros(shape=(12, 133, 3), dtype=np.float32)
+
+checkpoint_path = 'best.pth'
+device = 'cpu'
+cfg_path = 'phoenix-2014t.yaml'
+
+predictor = Predictor(cfg_path, checkpoint_path, device)
+glosas = predictor(keypoints)
+
+print(glosas) # Let's print the output to see the result
